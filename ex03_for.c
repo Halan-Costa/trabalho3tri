fc48fedf8d7b7d833f6b3598454e4a6f1875be38
Halan-Costa <halan.mine@gmail.com>
2025-11-03 08:29:59 -0300

    Create ex03_for.c

---
+++ ex03_for.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    int numero, resultado1, resultado2,resultado3,resultado4,resultado5,resultado6,resultado7,resultado8,resultado9,resultado10;
+    printf("digite um numero para ver sua tabuada\n");
+    scanf (" %d", &numero);
+    for (int i=0;i<10;i++);
+    (resultado1=numero*1);
+    printf(" a tabuada do seu numero é:\n %dx1=%d\n", numero,resultado1);
+    (resultado2=numero*2);
+    printf(" %dx2=%d\n", numero,resultado2);
+    (resultado3=numero*3);
+    printf(" %dx3=%d\n", numero,resultado3);
+    (resultado4=numero*4);
+    printf(" %dx4=%d\n", numero,resultado4);
+    (resultado5=numero*5);
+    printf(" %dx5=%d\n", numero,resultado5);
+    (resultado6=numero*6);
+    printf(" %dx6=%d\n", numero,resultado6);
+    (resultado7=numero*7);
+    printf(" %dx7=%d\n", numero,resultado7);
+    (resultado8=numero*8);
+    printf(" %dx8=%d\n", numero,resultado8);
+    (resultado9=numero*9);
+    printf(" %dx9=%d\n", numero,resultado9);
+    (resultado10=numero*10);
+    printf(" %dx10=%d\n", numero,resultado10);
+    
+}
